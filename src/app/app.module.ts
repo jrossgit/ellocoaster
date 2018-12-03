e7892731b4e0a741f3a5565d728a8e41ebcbfa22
jrossgit <james.i.ross5@gmail.com>
2018-12-03 22:49:11 +0000

    WIP start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EcSelectorComponent } from './ec-selector/ec-selector.component';
+import { EcMenuComponent } from './ec-menu/ec-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcSelectorComponent,
+    EcMenuComponent
   ],
   imports: [
     BrowserModule
